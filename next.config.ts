3761318bab3a5c93d7bf1bc0c609b23bd597c9d7
shielamae02 <111366401+shielamae02@users.noreply.github.com>
2024-11-22 21:50:42 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
    output: 'export',
+   basePath: '/NextJS-GithubActions', // Replace <repository-name> with your GitHub repository name
+   assetPrefix: '/NextJS-GithubActions/',
 };
 
 export default nextConfig;
