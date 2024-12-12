05bac4ca43db1356a76afb0b72ca50c7b860a674
shielamae02 <111366401+shielamae02@users.noreply.github.com>
2024-12-12 22:16:33 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,7 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
+          <li className="mb-2"
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
